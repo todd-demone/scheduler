844b7c6c011f0dc9ccd076d5f407aed99a295a5a
Todd Demone <todd.demone.dev@gmail.com>
2022-02-23 10:50:56 -0500

    create and export component; import .scss; refactor to add props including conditional styling and click handler; create helper function formatSpots() to render the text for # of spots remaining

---
+++ src/components/DayListItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import classNames from 'classnames';
+import 'components/DayListItem.scss';
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0, 
+  });
+
+  const formatSpots = () => {
+    if (props.spots === 0) {
+      return 'no spots remaining';
+    } else if (props.spots === 1) {
+      return '1 spot remaining'; 
+    } 
+    return `${props.spots} spots remaining`;
+  }
+
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  )
+}
