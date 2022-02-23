cac32a1f28d89b6f975dcbd9f329350e2935de3e
Todd Demone <todd.demone.dev@gmail.com>
2022-02-23 10:45:40 -0500

    add CSS classNames conditionally using classnames library; update return value to include props - classes, onClick handler, disabled attribute and props.children (text)

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+
+  const buttonClass = classNames({
+    "button": true,
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+  
+  return (
+    <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
+
 }
