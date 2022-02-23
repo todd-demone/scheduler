29467f8d83abaf4d1a3679c68a043734d766ea5a
Todd Demone <todd.demone.dev@gmail.com>
2022-02-23 10:56:21 -0500

    create and export function, import scss, add props, add click handler, conditionally create classnames, conditionally add interviewer name

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import classNames from 'classnames';
+import 'components/InterviewerListItem.scss';
+
+function InterviewerListItem(props) {
+  const interviewerClasses = classNames({
+    "interviewers__item": true,
+    "interviewers__item--selected": props.selected,
+  });
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewerClasses}>
+      <img 
+        src={props.avatar}
+        alt={props.name}
+        className="interviewers__item-image"
+      />
+      {props.selected && `${props.name}`}
+    </li>
+  );
+}
+
+export default InterviewerListItem;
