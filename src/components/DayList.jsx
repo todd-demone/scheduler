fb9c0c5add9d867f81c570b3f39f1e2864c10451
Todd Demone <todd.demone.dev@gmail.com>
2022-02-23 10:54:30 -0500

    use Array.map to create a list of DayListItems

---
+++ src/components/DayList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem';
+
+function DayList(props) {
+
+  // create an array of DayListItems
+  // take props.days (object) and turn into array of values
+  // create a new array of DayListItem components, passing name, spots, selected and setDay to each component
+  const arrayOfDayListItems = props.days.map((day) => 
+    <DayListItem 
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay} 
+    />)
+  return (
+    <ul>
+      {arrayOfDayListItems}
+    </ul>
+  );
+}
+
+export default DayList;
