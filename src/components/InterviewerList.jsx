cfe8be647af681f34599c8f1dba3bee8b5900fb9
Todd Demone <todd.demone.dev@gmail.com>
2022-02-24 09:44:07 -0500

    Complete 'Controlled Lists' exercise - change interviewer to value and change setInterviewer to onChange

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -3,13 +3,18 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import 'components/InterviewerList.scss';
 
 function InterviewerList(props) {
+  // While completing "Controlled Lists", I had to add this line to make the stories work:
+  const onChange = props.onChange;
+  const value = props.value;
   const arrayOfInterviewers = props.interviewers.map((interviewer => {
     return (<InterviewerListItem
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={props.interviewer === interviewer.id}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      // selected={props.interviewer === interviewer.id}
+      selected={interviewer.id === value}
+      // setInterviewer={() => props.setInterviewer(interviewer.id)}
+      setInterviewer={() => onChange(interviewer.id)}
     />);
   }));
   
