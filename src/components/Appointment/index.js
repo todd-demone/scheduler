7bf04585b6342d62bedf52fe7829d4be2c135359
Todd Demone <todd.demone.dev@gmail.com>
2022-02-25 14:40:36 -0500

    modify Appointment/index.js to render Header child component and conditionally render Show or Empty child components; test with Storybook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Header from 'components/Appointment/Header'
+import Show from './Show';
+import Empty from './Empty';
+
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  const message = props.time ? `Appointment at ${props.time}`: "No appointments";
   return (
-    <article className="appointment">{message}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      { // check if an interview object was passed
+        (props.interview) 
+          ? <Show 
+              student={props.interview.student}
+              interviewer={props.interview.interviewer}
+              onEdit={props.onEdit}
+              onDelete={props.onDelete}
+            /> 
+          : <Empty onAdd={props.onAdd} />
+      }
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -199,5 +199,23 @@ storiesOf("DayListItem", module)
             onSave={action("onSave")}
             onCancel={action("onCancel")}
           />
+        ))
+        .add("Appointment Empty", () => (
+          <Fragment>
+            <Appointment id={1} time="4pm" onAdd={action("onAdd")} />
+            <Appointment time="5pm" />
+          </Fragment>
+        ))
+        .add("Appointment Booked", () => (
+          <Fragment>
+            <Appointment
+              id={1}
+              time="4pm"
+              interview={{ student: "Lydia Miller-Jones", interviewer }}
+              onEdit={action("onEdit")}
+              onDelete={action("onDelete")}
+            />
+            <Appointment time="5pm" />
+          </Fragment>
         ));
         
