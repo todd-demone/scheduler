1683e0e7b763e7e0b67f9330da7653c296a1f17a
Todd Demone <todd.demone.dev@gmail.com>
2022-02-25 11:36:50 -0500

    add Appointment views - Show, Confirm, Status, Error; add stories for each view

--- src/components/Appointment/Empty.jsx
+++ src/components/Appointment/Empty.jsx
@@ -4,9 +4,9 @@ export default function Empty(props) {
   return (
     <main className="appointment__add">
       <img 
+        className="appointment__add-button"
         src="images/add.png"
         alt="Add"
-        className="appointment__add-button"
         onClick={props.onAdd}
       />
     </main>
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index.js';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,4 +156,30 @@ storiesOf("DayListItem", module)
         ))
         .add("Empty", () => (
           <Empty onAdd={action("onAdd")} />
+        ))
+        .add("Show", () => (
+          <Show
+            student="Lydia Miller-Jones"
+            interviewer={interviewer}
+            onEdit={action("onEdit")}
+            onDelete={action("onDelete")}
+          />
+        ))
+        .add("Confirm", () => (
+          <Confirm
+            message="Delete the appointment?"
+            onCancel={action("onCancel")}
+            onConfirm={action("onConfirm")}
+          />
+        ))
+        .add("Status", () => (
+          <Status
+            message="Deleting"
+          />
+        ))
+        .add("Error", () => (
+          <Error 
+            message="Could not delete appointment"
+            onClose={action("onClose")}
+          />
         ));
