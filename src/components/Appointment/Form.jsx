6cbafbc8c49fe53347bfd069480891ecc4b9be4d
Todd Demone <todd.demone.dev@gmail.com>
2022-02-25 14:05:01 -0500

    add event.preventDefault() on form submit; add props.onSave to Confirm Button

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -19,7 +19,7 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -39,7 +39,7 @@ export default function Form(props) {
         <section className="appointment__actions">
           {/* ADD RESET() etc. */}
           <Button onClick={cancel} danger >Cancel</Button>
-          <Button confirm >Save</Button>
+          <Button onClick={props.onSave} confirm >Save</Button>
         </section>
       </section>
     </main>
