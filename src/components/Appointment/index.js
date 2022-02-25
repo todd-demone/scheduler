90836494ceeea6a7db235dec82c41b86a1880769
Todd Demone <todd.demone.dev@gmail.com>
2022-02-25 11:02:05 -0500

    use ternary operator to display Appointment message

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  let message = "No appointments";
-  if (props.time) message = `Appointment at ${props.time}`;
+  const message = props.time ? `Appointment at ${props.time}`: "No appointments";
   return (
     <article className="appointment">{message}</article>
   );
