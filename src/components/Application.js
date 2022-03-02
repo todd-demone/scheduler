fd5f1e0dfc3a977cb2f84271802e7c6a9794806b
Todd Demone <todd.demone.dev@gmail.com>
2022-03-02 14:32:27 -0500

    make http request for days using axios and useEffect; remove hard-coded days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment'
-
+import axios from 'axios';
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,7 +44,7 @@ const appointments = {
 };
 
 const appointmentsArray = Object.values(appointments).map((appointment) => (
-  <Appointment 
+  <Appointment
     key={appointment.id}
     {...appointment}
     onEdit={() => console.log('onEdit')}
@@ -75,6 +57,14 @@ appointmentsArray.push(<Appointment key="last" time="5pm" />);
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/days')
+      .then((response) => {
+        setDays(response.data);
+      })
+  }, []);
 
   return (
     <main className="layout">
@@ -103,6 +93,6 @@ export default function Application(props) {
       </section>
 
     </main>
-    
+
   );
 }
