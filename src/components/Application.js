f1bc154e2ee71f25e00ccef66ccf23b2aee50e09
Todd Demone <todd.demone.dev@gmail.com>
2022-03-03 12:39:10 -0500

    create selector called getInterview (helper function) to add interviewer object as a property to appointment object; create tests for new selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,38 +2,49 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment'
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
   const setDay = (day) => setState({...state, day});
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const appointmentsArray = dailyAppointments.map((appointment) =>
-    (<Appointment
-      key={appointment.id}
-      {...appointment}
-      onEdit={() => console.log('onEdit')}
-      onDelete={() => console.log("onDelete")}
-      onAdd={() => console.log("onAdd")}
-    />)
-  );
+  const appointmentsArray = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id= {appointment.id}
+        time={appointment.time}
+        interview={interview}
+        onEdit={() => console.log('onEdit')}
+        onDelete={() => console.log("onDelete")}
+        onAdd={() => console.log("onAdd")}
+      />
+    );
+  });
   appointmentsArray.push(<Appointment key="last" time="5pm" />);
 
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
     .then((all) => {
+      console.log(all[1].data)
+      console.log(all[2].data)
       setState((prev) => ({
         ...prev,
         days: all[0].data,
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }));
     })
   }, []);
