9396e38b21df7893ddb960a8fcc91cf7e5537d4b
Todd Demone <todd.demone.dev@gmail.com>
2022-03-08 10:50:55 -0500

    add getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,6 @@
 export function getAppointmentsForDay(state, day) {
-  // return an empty array when the days data is empty
   if (state.days.length === 0) return [];
   const dayObject = state.days.find((dayItem) => dayItem.name === day);
-  // return an empty array when the day is not found
   if (!dayObject) return [];
   return Object.values(state.appointments).filter((appointment) => dayObject.appointments.includes(appointment.id));
 };
@@ -13,4 +11,11 @@ export function getInterview(state, interview) {
     student: interview.student,
     interviewer: state.interviewers[interview.interviewer]
   };
+};
+
+export function getInterviewersForDay(state, day) {
+  if (state.days.length === 0) return [];
+  const dayObject = state.days.find((dayItem) => dayItem.name === day);
+  if (!dayObject) return [];
+  return Object.values(state.interviewers).filter((interviewer) => dayObject.interviewers.includes(interviewer.id));
 };
