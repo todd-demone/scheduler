557ade364e25f1fe782678a1eef16b6a27f30d88
Todd Demone <todd.demone.dev@gmail.com>
2022-03-08 21:01:58 -0500

    add EDIT mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,6 +15,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT="EDIT";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -51,6 +52,7 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onEdit={() => transition(EDIT)}
           onDelete={() => transition(CONFIRM)}
         />
       )}
@@ -68,6 +70,15 @@ export default function Appointment(props) {
           message="Are you sure you would like to delete?"
           onCancel={() => back()}
           onConfirm={() => remove(props.id)}
+        />
+      )}
+      {mode === "EDIT" && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          interviewers={props.interviewers}
+          onCancel={() => back()}
+          onSave={save}
         />
       )}
       {/* { // check if an interview object was passed
